1da87c77cb08242a4bb25af859cb2784aa239231
Rakesh <mondalrakesh461@gmail.com>
2025-05-02 23:07:57 +0530

    Object Merging

--- src/scripts/exercise3.ts
+++ src/scripts/exercise3.ts
@@ -1,5 +1,5 @@
 // ------------------- Discriminated Union ------------------ //
-
+/*
 type FormField =
   | { type: "text"; value: string }
   | { type: "checkbox"; value: boolean }
@@ -16,3 +16,55 @@ function processField(field: FormField): void {
 }
 
 // ------------------- Discriminated Union ------------------ //
+
+const defaultSettings = {
+  theme: "light",
+  notifications: true,
+};
+
+const userSettings = {
+  theme: "dark",
+  fontSize: "medium",
+};
+
+function mergeSettings<T, U>(obj1: T, obj2: U): T & U {
+  return { ...obj1, ...obj2 };
+}
+
+const finalSettings = mergeSettings(defaultSettings, userSettings);
+
+
+
+const defaultConfig = {
+  theme: "light",
+  fontSize: "16px",
+  language: "English",
+};
+
+const userConfig = {
+  theme: "dark",
+  language: "Spanish",
+};
+
+function mergeConfig<U, V>(obj1: U, obj2: V): U & V {
+  return { ...obj1, ...obj2 };
+}
+
+const finalConfig = mergeConfig(defaultConfig, userConfig);
+
+
+console.log("finalConfig✅✅",finalConfig);
+
+*/
+
+function mergeArray<A>(arr1:A[], arr2:A[]):A[]{
+  return Array.from(new Set([...arr1,...arr2]))
+}
+
+
+const arr1 = [1, 2, 3, 4];
+const arr2 = [3, 4, 5, 6];
+
+const mergedArray = mergeArray(arr1, arr2);
+
+console.log("mergedArray✅✅",mergedArray);
